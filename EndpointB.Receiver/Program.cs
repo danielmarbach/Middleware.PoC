43a2e87a313a8d315eaceb140f4a99aa2a3fe586
danielmarbach <daniel.marbach@openplace.net>
2017-11-30 18:12:23 +0100

    Processing endpoints

---
+++ EndpointB.Receiver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace EndpointA.Receiver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var endpointConfiguration = new EndpointConfiguration("EndpointB.Receiver");
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+
+            endpointConfiguration.SendFailedMessagesTo("error");
+
+            var endpoint = await Endpoint.Start(endpointConfiguration);
+
+            Console.ReadLine();
+        }
+    }
+}
